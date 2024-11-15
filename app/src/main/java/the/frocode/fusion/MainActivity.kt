ecfd67bb786c484aa8c0c90ad323b6535f051838
The Fro Code <cmukiri808@gmail.com>
2024-11-15 13:15:22 +0300

    Initial Commit

---
+++ app/src/main/java/the/frocode/fusion/MainActivity.kt
@@ -0,0 +1,47 @@
+package the.frocode.fusion
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import the.frocode.fusion.ui.theme.FusionTheme
+import the.frocode.super_app_sdk.TestMain
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FusionTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TestMain()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FusionTheme {
+        Greeting("Android")
+    }
+}
