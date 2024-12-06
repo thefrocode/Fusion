57c3132bf81bec6b56c561717c957e77a6b98bb2
The Fro Code <cmukiri808@gmail.com>
2024-12-06 19:27:41 +0300

    Update Miniapp Entity

--- super-app-sdk/src/main/java/the/frocode/super_app_sdk/MiniApp.kt
+++ super-app-sdk/src/main/java/the/frocode/super_app_sdk/MiniApp.kt
@@ -3,7 +3,11 @@ package the.frocode.super_app_sdk
 import androidx.navigation.NavController
 import the.frocode.super_app_sdk.internals.nav.openWebview
 
-class MiniApp(val app_url: String, val name: String) {
+class MiniApp(val app_url: String,
+              val name: String,
+              val category_id: Int,
+              val icon_url: String,
+              val build_url: String?) {
 
     fun openMiniApp(navController: NavController) {
         println("Opening Mini App")
